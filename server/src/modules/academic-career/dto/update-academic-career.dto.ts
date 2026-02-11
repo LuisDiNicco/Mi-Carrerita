8aab0adb636adf861ecaf43b320c2c91d3dc1f7c
LuisDiNicco <luisdini7800@gmail.com>
2026-02-11 18:52:15 -0300

    refactor: remove unused files

--- server/src/modules/academic-career/dto/update-academic-career.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAcademicCareerDto } from './create-academic-career.dto';
-
-export class UpdateAcademicCareerDto extends PartialType(CreateAcademicCareerDto) {}
